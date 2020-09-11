4b78263855a9b483cf783b98cb668645e040b48c
Dan <daniel@spyclub.co.uk>
2020-09-11 23:34:19 +0100

    Fitted to 76 col

--- src/ValispaceInit.m
+++ src/ValispaceInit.m
@@ -1,35 +1,54 @@
-function ValispaceInit(URL,Username,Password,insecure)
+function ValispaceInit(URL, Username, Password, insecure)
+% Initialise Valispace and connect to API
 
 % check for SSL connection, before sending the username and password
     if (strncmpi(URL,'http://',7))
+        passwordWarning = ['Sending password-credentials ' ...
+            'using an unencrypted connection. '...
+            'Better to use "https://..." instead!'];
+            
         if (exist('insecure', 'var') && strcmp(insecure, 'insecure'))
-            warning('VALISPACE-WARNING: You are sending your password-credentials using an unencrypted connection. Better use "https://..." instead!');
+            warning(['VALISPACE-WARNING: ' passwordWarning]);
         else
-            error('VALISPACE-ERROR: You are trying to send your password-credentials using an unencrypted connection. Better use "https://..." instead! If you still want to go ahead with an insecure connection, please use ValisapceInit() with "insecure" as a last argument.');
+            error(['VALISPACE-ERROR: ' passwordWarning ...
+                ' To still use insecure connection, use ValisapceInit ' ...
+                'with "insecure" as the last argument.']);
         end
     end
 
-
-%ValispaceInit performs the password based oAuth 2.0 login for resd/write access
+%Perform password based oAuth 2.0 login for read/write access
     if (URL(end)=='/')
         BasicUrl = URL(1:end-1);
     else
         BasicUrl = URL;
     end
     oAuthUrl = strcat(BasicUrl, '/o/token/');
-    client_id = 'docs.valispace.com/user-guide/addons/#matlab'; % registered client-id in Valispace Deployment
-    result = webwrite(oAuthUrl,'grant_type','password','username',Username,'password',Password,'client_id',client_id);
+    % registered client-id in Valispace Deployment
+    client_id = 'docs.valispace.com/user-guide/addons/#matlab'; 
+    result = webwrite(oAuthUrl, ...
+        'grant_type', 'password', ...
+        'username', Username, ...
+        'password', Password, ...
+        'client_id', client_id);
     
     access = horzcat('Bearer ', result.access_token);
     
     global ValispaceLogin;
-    if verLessThan('matlab', '9.1')    % HeaderFields was introduced in 2016b (9.1)
-        ValispaceLogin.options = weboptions('Timeout', 200, 'ContentType', 'json', 'KeyName', 'Authorization', 'KeyValue', access);
+    % HeaderFields was introduced in 2016b (9.1)
+    if verLessThan('matlab', '9.1')
+        ValispaceLogin.options = weboptions('Timeout', 200, ...
+            'ContentType', 'json', ...
+            'KeyName', 'Authorization', ...
+            'KeyValue', access);
     else
-        ValispaceLogin.options = weboptions('Timeout', 200, 'HeaderFields', {'Authorization' access; 'Content-Type' 'application/json'});
+        ValispaceLogin.options = weboptions('Timeout', 200, ...
+            'HeaderFields', ...
+            {'Authorization' access; ...
+            'Content-Type' 'application/json'});
     end
     
     ValispaceLogin.url = strcat(BasicUrl, '/rest/'); 
 
-    fprintf('VALISPACE: You have been successfully connected to the %s API.\n', ValispaceLogin.url);
+    fprintf('VALISPACE: Successfully connected to the %s API.\n', ...
+        ValispaceLogin.url);
 end
