e641136e8046d07b8eb6e2d82a1922df8adbf0bc
Dan <daniel@spyclub.co.uk>
2020-09-11 22:45:33 +0100

    Prefix with VALISPACE-WARNING

--- src/ValispaceInit.m
+++ src/ValispaceInit.m
@@ -3,7 +3,7 @@ function ValispaceInit(URL,Username,Password,insecure)
 % check for SSL connection, before sending the username and password
     if (strncmpi(URL,'http://',7))
         if (exist('insecure', 'var') && strcmp(insecure, 'insecure'))
-            warning('You are sending your password-credentials using an unencrypted connection. Better use "https://..." instead!');
+            warning('VALISPACE-WARNING: You are sending your password-credentials using an unencrypted connection. Better use "https://..." instead!');
         else
             error('VALISPACE-ERROR: You are trying to send your password-credentials using an unencrypted connection. Better use "https://..." instead! If you still want to go ahead with an insecure connection, please use ValisapceInit() with "insecure" as a last argument.');
         end
