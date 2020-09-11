4b324754a09ec08807de39532f1ce4439b2343b7
Dan <daniel@spyclub.co.uk>
2020-09-12 00:17:42 +0100

    Offline cache option

--- src/ValispacePull.m
+++ src/ValispacePull.m
@@ -1,16 +1,28 @@
-function [ ValiList ] = ValispacePull()
+function [ ValiListOut, ValiMatrixListOut ] = ValispacePull()
 %ValispaceAllValis Returns a list of all Valis and stores it globally
     global ValispaceLogin;
     global ValiList;
     global ValiMatrixList;
     
-    if (isempty(ValispaceLogin))
-        error('VALISPACE-ERROR: You first have to run ValispaceInit()');
+    cachefile = 'ValispacePullCache.mat';
+    
+    if (isempty(ValispaceLogin) && ~isfile(cachefile))
+        error(['VALISPACE-ERROR: You first have to run ValispaceInit() '...
+            'or have a offline ' cachefile ' file.']);
+    elseif (isempty(ValispaceLogin) && isfile(cachefile))
+        load(cachefile, 'ValiList', 'ValiMatrixList');
+    else
+        options = ValispaceLogin.options;
+        
+        url = strcat(ValispaceLogin.url, 'valis/');
+        ValiList = rot90(webread(url,options));
+        
+        url2 = strcat(ValispaceLogin.url, 'matrices/');
+        ValiMatrixList = rot90(webread(url2,options));
+        
+        save(cachefile, 'ValiList', 'ValiMatrixList'); 
     end
     
-    options = ValispaceLogin.options;
-    url = strcat(ValispaceLogin.url, 'valis/');
-    ValiList = rot90(webread(url,options));
-    url2 = strcat(ValispaceLogin.url, 'matrices/');
-    ValiMatrixList = rot90(webread(url2,options));
+    ValiListOut = ValiList;
+    ValiMatrixListOut = ValiMatrixList;
 end
